82a78b787539de958571ce7980010692d1c42c4d
alysson-Scott <alysson120107@gmail.com>
2024-08-28 08:43:20 -0300

    tccteste

--- src/templates/Login/Login.jsx
+++ src/templates/Login/Login.jsx
@@ -4,13 +4,13 @@ import Maromba from '../../assets/home2.jpg'
 
 const Login = () => {
   return (
-       <div className='w-full h-screen flex items-start'>
+       <div className='w-full h-full flex items-start'>
         <div className='relative w-1/2 h-full flex flex-col'>
           <div className='absolute-top-[20%] left-[10%] flex flex-col'>
-            <h1 className='text-4xl text-black font-bold my-4'>Nutrindo hoje os atletas de amanhã</h1>
-            <p className='text-xl text-black font-normal'>Nós garantimos os melhores suplementos para a melhor performance</p>
+            <h1 className='text-2xl text-black font-bold my-4'>Nutrindo hoje os atletas de amanhã</h1>
+            <p className='text-4xl text-black font-normal'>Nós garantimos os melhores suplementos para a melhor performance</p>
           </div>
-          <img src={Maromba}  className='w-full h-full object-cover'/>
+          <img src={Maromba} id='Maromba'/>
         </div>
  
          <div className='w-1/2 h-full bg-black flex flex-col'>
