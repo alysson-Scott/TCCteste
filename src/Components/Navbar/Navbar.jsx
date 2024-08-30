51bfbb2c5bb478aaf4a8cd098abb266f2cfbb7b0
Alysson Scott Zanardi Dias <alysson120107@gmail.com>
2024-08-30 16:33:27 -0300

    navbar responsiva

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import kitFit from '../../assets/KitFit.png';
@@ -6,8 +6,10 @@ import Button from './button';
 
 
 
+
 const Navbar = () => {
-  
+    
+    let [open,setOpen]=useState(false);
 
     return (
         <div className='shadow-md w-full fixed top-0 left-0 '>
@@ -17,10 +19,10 @@ const Navbar = () => {
                     </span>
                     <img src={kitFit} className='h-[50px] md:ml-5 ' />
                 </div>
-                <div>
-                <ion-icon name="menu"></ion-icon>
+                <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden pt-3'>
+                <ion-icon name={open ? 'close':'menu-outline'}></ion-icon>
                 </div>
-                <ul className='text-white md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in'>
+                <ul className={`text-white md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100':'top-[-490px]'} md:opacity-100 `}>
                     <li className='md:ml-8 text-xl md:my-0 my-7'><a href="#inicioscroll"className="hover:text-orange duration-500">Início</a></li>
                     <li className='md:ml-8 text-xl md:my-0 my-7'><a href="#aboutscroll" className="hover:text-orange duration-500">Sobre nós</a></li>
                     <li className='md:ml-8 text-xl md:my-0 my-7'><a href='#contatoscroll' className="hover:text-orange duration-500">Contato</a></li>
