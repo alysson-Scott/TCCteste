0e586dbb109e7176a77b66d39d8c3dde0dd9d30e
alysson-Scott <alysson120107@gmail.com>
2024-09-06 09:52:09 -0300

    bug

--- src/Components/Navbar/button.jsx
+++ src/Components/Navbar/button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = (props) =>{
     return(
-        <button className="bg-orange text-white py-2 px-4 rounded md:ml-8  hover:bg-white hover:text-orange duration-500 mr-4">
+        <button className="bg-orange text-white py-2 px-4 rounded md:ml-8  hover:bg-white hover:text-orange duration-500">
             {props.children}
         </button>
     )
--- src/templates/Inicio/Inicio.jsx
+++ src/templates/Inicio/Inicio.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import './Inicio.css'
+import Carousel from './carousel'
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 const Home = () => {
+  
   return (
-   
-    <div className='home container' id='inicioscroll'>
-      <div className="home-text">
-        <h1>Nós garantimos os melhores suplementos para a melhor performance</h1>
-      </div>
-    </div>
+   <Carousel></Carousel>
   )
 }
 
